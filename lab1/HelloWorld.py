5e3c3e06a348564e9358cd6047b3a3ba60bb5bda
Turnip Kogaku <turnipkogaku@gmail.com>
2024-04-18 16:17:00 +0900

    HelloWorld.py

--- lab1/HelloWorld.py
+++ lab1/HelloWorld.py
@@ -1,1 +1,9 @@
-Print ("Hello World - ET0735, DCPE")
+import webbrowser
+
+# Prints random shit on console
+print ("Hello World - ET0735, DCPE")
+
+# Opens URL
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+webbrowser.open(url, new=0, autoraise=True)
